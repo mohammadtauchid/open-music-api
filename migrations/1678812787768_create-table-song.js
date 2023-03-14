a53f5646acb9da97d55da268952c14ad6170622e
Tauchid <muhammadtauchid@gmail.com>
2023-03-15 00:20:33 +0700

    add migration for song table

---
+++ migrations/1678812787768_create-table-song.js
@@ -0,0 +1,36 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable('songs', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    genre: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    performer: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    duration: {
+      type: 'SMALLINT',
+      allowNull: true,
+    },
+    albumId: {
+      type: 'VARCHAR(50)',
+      allowNull: true,
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('songs');
+};
