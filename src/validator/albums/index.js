c4d985572046c4da79c52d36c3b4e78596a0bab6
Tauchid <muhammadtauchid@gmail.com>
2023-04-11 18:09:11 +0700

    fix bug

--- src/validator/albums/index.js
+++ src/validator/albums/index.js
@@ -1,5 +1,5 @@
 const InvariantError = require('../../exceptions/InvariantError');
-const { AlbumPayloadSchema } = require('./schema');
+const AlbumPayloadSchema = require('./schema');
 
 const AlbumsValidator = {
   validateAlbumPayload: (payload) => {
